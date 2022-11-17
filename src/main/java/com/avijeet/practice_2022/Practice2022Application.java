4c7015016926573fd507e0316d9576eb353d8b6a
Avijeet <33360737+Avijeet4@users.noreply.github.com>
2022-11-17 12:28:29 +0530

    Initial commit

---
+++ src/main/java/com/avijeet/practice_2022/Practice2022Application.java
@@ -0,0 +1,13 @@
+package com.avijeet.practice_2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practice2022Application.class, args);
+	}
+
+}
